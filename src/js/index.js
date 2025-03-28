205ce61ae97ba4603bc57978ba2d2527563fb59a
Vasiliy Igonin <vigonin83@gmail.com>
2025-03-28 21:23:53 +0300

    init project and develop header & footer

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import "./libs/accordion.js"
+import './libs/modal.js'
+import './libs/scroll.js'
+import './libs/slider.js'
+import './libs/mobileMenu.js'
+
+console.log('Project has been started...')
